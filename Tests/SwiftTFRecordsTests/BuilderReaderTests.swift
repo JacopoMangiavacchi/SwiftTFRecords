85275f6847e55e7afc120f7b852d294e95f514fa
Jacopo Mangiavacchi <jamangia@microsoft.com>
2019-11-03 11:01:06 -0800

    Minor fix tests

--- Tests/SwiftTFRecordsTests/BuilderReaderTests.swift
+++ Tests/SwiftTFRecordsTests/BuilderReaderTests.swift
@@ -6,15 +6,15 @@ final class BuilderReaderTests: XCTestCase {
         var record1 = Record()
         var record2 = Record()
 
-        record1.set("Int1", feature: 1)
-        record1.set("Float2.3", feature: 2.3)
-        record1.set("Bytes1234", feature: Feature.Bytes(Data([1, 2, 3, 4])))
-        record1.set("StringJacopo", feature: "Jacopo 😃")
+        record1.set("Int", feature: 1)
+        record1.set("Float", feature: 2.3)
+        record1.set("Bytes", feature: Feature.Bytes(Data([1, 2, 3, 4])))
+        record1.set("String", feature: "Jacopo 😃")
 
-        record2.set("Int1", feature: 2)
-        record2.set("Float2.3", feature: 4.6)
-        record2.set("Bytes1234", feature: Feature.Bytes(Data([5, 6, 7, 8])))
-        record2.set("StringJacopo", feature: "Jacopo 😃😃")
+        record2.set("Int", feature: 2)
+        record2.set("Float", feature: 4.6)
+        record2.set("Bytes", feature: Feature.Bytes(Data([5, 6, 7, 8])))
+        record2.set("String", feature: "Jacopo 😃😃")
 
         let builder = Builder(withRecords: [record1, record2])
         let tfRecordIn = builder.data
@@ -23,15 +23,15 @@ final class BuilderReaderTests: XCTestCase {
         
         XCTAssertEqual(reader.records.count, 2)
 
-        XCTAssertEqual(reader.records[0].context["Int1"]?.toInt(), 1)
-        XCTAssertEqual(reader.records[0].context["Float2.3"]?.toFloat(), 2.3)
-        XCTAssertEqual(reader.records[0].context["Bytes1234"]?.toBytes(), Data([1, 2, 3, 4]))
-        XCTAssertEqual(reader.records[0].context["StringJacopo"]?.toString(), "Jacopo 😃")
+        XCTAssertEqual(reader.records[0].context["Int"]?.toInt(), 1)
+        XCTAssertEqual(reader.records[0].context["Float"]?.toFloat(), 2.3)
+        XCTAssertEqual(reader.records[0].context["Bytes"]?.toBytes(), Data([1, 2, 3, 4]))
+        XCTAssertEqual(reader.records[0].context["String"]?.toString(), "Jacopo 😃")
 
-        XCTAssertEqual(reader.records[1].context["Int1"]?.toInt(), 2)
-        XCTAssertEqual(reader.records[1].context["Float2.3"]?.toFloat(), 4.6)
-        XCTAssertEqual(reader.records[1].context["Bytes1234"]?.toBytes(), Data([5, 6, 7, 8]))
-        XCTAssertEqual(reader.records[1].context["StringJacopo"]?.toString(), "Jacopo 😃😃")
+        XCTAssertEqual(reader.records[1].context["Int"]?.toInt(), 2)
+        XCTAssertEqual(reader.records[1].context["Float"]?.toFloat(), 4.6)
+        XCTAssertEqual(reader.records[1].context["Bytes"]?.toBytes(), Data([5, 6, 7, 8]))
+        XCTAssertEqual(reader.records[1].context["String"]?.toString(), "Jacopo 😃😃")
     }
 
     static var allTests = [
--- Tests/SwiftTFRecordsTests/RecordTests.swift
+++ Tests/SwiftTFRecordsTests/RecordTests.swift
@@ -5,19 +5,19 @@ final class RecordTests: XCTestCase {
     func testPersistRecord() {
         var recordIn = Record()
         
-        recordIn.set("Int1", feature: 1)
-        recordIn.set("Float2.3", feature: 2.3)
-        recordIn.set("Bytes1234", feature: Feature.Bytes(Data([1, 2, 3, 4])))
-        recordIn.set("StringJacopo", feature: "Jacopo 😃")
+        recordIn.set("Int", feature: 1)
+        recordIn.set("Float", feature: 2.3)
+        recordIn.set("Bytes", feature: Feature.Bytes(Data([1, 2, 3, 4])))
+        recordIn.set("String", feature: "Jacopo 😃")
 
         guard let data = recordIn.data else { return XCTFail() }
         
         let recordOut = Record(withData: data)
         
-        XCTAssertEqual(recordOut.get("Int1")?.toInt(), 1)
-        XCTAssertEqual(recordOut.get("Float2.3")?.toFloat(), 2.3)
-        XCTAssertEqual(recordOut.get("Bytes1234")?.toBytes(), Data([1, 2, 3, 4]))
-        XCTAssertEqual(recordOut.get("StringJacopo")?.toString(), "Jacopo 😃")
+        XCTAssertEqual(recordOut.get("Int")?.toInt(), 1)
+        XCTAssertEqual(recordOut.get("Float")?.toFloat(), 2.3)
+        XCTAssertEqual(recordOut.get("Bytes")?.toBytes(), Data([1, 2, 3, 4]))
+        XCTAssertEqual(recordOut.get("String")?.toString(), "Jacopo 😃")
     }
 
     static var allTests = [
